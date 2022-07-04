a2b23e1a49e8683f6975b56efc79537829df6d40
ItsTheGuy <36568572+ItsTheGuy@users.noreply.github.com>
2022-07-04 17:09:29 +0200

    Changed license banner

--- Helpers/Data.swift
+++ Helpers/Data.swift
@@ -1,5 +1,5 @@
 // NoteFlier
-// Copyright (C) 2022 ItsTheGuy
+// Copyright (C) 2022 Oscar
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
